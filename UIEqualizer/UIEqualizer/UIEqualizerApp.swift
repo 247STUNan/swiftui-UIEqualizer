ec5db39bc34070398b7ec8abf3a9c1a4e718d888
Parrot Blue <>
2025-06-03 20:37:01 +0700

    #3-6 "equalizer ui"

---
+++ UIEqualizer/UIEqualizer/UIEqualizerApp.swift
@@ -0,0 +1,12 @@
+// swiftui equalizer 🌟🌟🌟🌟🌟
+// githubs: https://github.com/247STUNan/swiftui-UIEqualizer
+// start: 3-6-2024
+import SwiftUI
+@main
+struct UIEqualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
